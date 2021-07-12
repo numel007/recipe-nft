7c303db794d2836ed521dff968f628e694a52207
Jackie <taekwondo.chan@gmail.com>
2021-07-11 17:58:44 -0700

    add _getOwner, _transferRecipe

--- contracts/RecipeContracts.sol
+++ contracts/RecipeContracts.sol
@@ -2,12 +2,13 @@ pragma solidity >=0.8.0;
 
 import "../node_modules/@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
 import "../node_modules/@openzeppelin/contracts/utils/Counters.sol";
+import "../node_modules/@openzeppelin/contracts/access/Ownable.sol";
 
-contract RecipeContract is ERC721URIStorage {
+contract RecipeContract is Ownable {
     using Counters for Counters.Counter;
     Counters.Counter private _tokenIds;
 
-    constructor() public ERC721("Recipe", "RCP") {}
+    // constructor() public ERC721("Recipe", "RCP") {}
 
     struct Recipe {
         string name;
@@ -18,7 +19,7 @@ contract RecipeContract is ERC721URIStorage {
     Recipe[] public recipes;
     mapping(uint256 => address) public recipeOwner;
 
-    function _createRecipe(string memory _name, string memory _method) private {
+    function _createRecipe(string memory _name, string memory _method) public {
         // Create recipe from params, set value to 1
         Recipe memory newRecipe = Recipe(_name, _method, 1);
 
@@ -28,5 +29,19 @@ contract RecipeContract is ERC721URIStorage {
 
         // Add recipe to dict, assign to msg.sender
         recipeOwner[id] = msg.sender;
+    }
+
+    function _getOwner(uint256 recipeId) public view returns (address) {
+        return recipeOwner[recipeId];
+    }
+
+    function _transferRecipe(
+        address oldOwner,
+        address newOwner,
+        uint256 recipeId
+    ) private returns (address) {
+        require(recipeOwner[recipeId] == oldOwner);
+        recipeOwner[recipeId] = newOwner;
+        return recipeOwner[recipeId];
     }
 }
